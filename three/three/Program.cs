067a78894083a6029f8057669be6569374d9c0b2
John O'Connor <crash7863@gmail.com>
2019-04-07 02:16:30 -0400

    reverse String

--- three/three/Program.cs
+++ three/three/Program.cs
@@ -3,7 +3,18 @@
 namespace three{
     class Program{
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a word!");
+            String userInput = Console.ReadLine();
+            reverseString(userInput);
+        }
+
+        static void reverseString(String userInput) {
+            Char[] word = userInput.ToCharArray();
+            for (int i = word.Length - 1; i > -1; i--) {
+                Console.Write(word[i]);
+            }
         }
     }
 }
+// h e l l o = 5;
+// 0 1 2 3 4
